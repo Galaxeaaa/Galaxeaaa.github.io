64e8066fa030a3c15bd7305f9c21a538555dca02
Galaxea@Athena <wangcheng1324@sina.com>
2022-03-13 23:58:20 +0800

    Modify site title.

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Cheng Wang'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Me=ChengWang'
 
 export default function Layout({ children, home }) {
 	return (
