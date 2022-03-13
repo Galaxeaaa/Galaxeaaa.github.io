77ecfe6323a62ff107a66add45df412d79929931
Galaxea@Athena <wangcheng1324@sina.com>
2022-03-13 23:28:27 +0800

    .PNG => .png

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-	assetPrefix: isProd ? '/galaxeaaa.github.io/' : '',
+	// assetPrefix: isProd ? '/Galaxeaaa.github.io/' : '',
 	images: {
 		loader: 'akamai',
 		path: '',
