b14ee0393ebadfc875c141b34f1666f9322ddac8
Galaxea@Athena <wangcheng1324@sina.com>
2022-03-13 23:18:34 +0800

    Modify next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-	assetPrefix: isProd ? '/mysite/' : '',
+	// assetPrefix: isProd ? '//' : '',
 	images: {
 		loader: 'akamai',
 		path: '',
