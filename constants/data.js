904db0ff8d9406c2bd55f8d9d028d2c680ba891f
galaxea <wangcheng1324@sina.com>
2022-03-14 22:40:27 +0800

    Modify.

--- constants/data.js
+++ constants/data.js
@@ -1,117 +1,101 @@
 const userData = {
-  githubUsername: "manuarora700",
-  name: "Manu Arora",
-  designation: "Full-Stack Developer",
-  avatarUrl: "/avatar.png",
-  email: "manuarorawork@gmail.com",
-  phone: "+91 9587738861",
-  address: "Jaipur, Rajasthan, India.",
-  projects: [
-    {
-      title: "Tailwind Master Kit",
-      link: "https://tailwindmasterkit.com",
-      imgUrl: "/tmk.jpg",
-    },
-    {
-      title: "PlaceholderTech",
-      link: "https://placeholdertech.in",
-      imgUrl: "/placeholdertech.png",
-    },
-    {
-      title: "Portfolio",
-      link: "https://manuarora.in",
-      imgUrl: "/portfolio.png",
-    },
-    {
-      title: "CovidRescue",
-      link: "https://covidrescue.co.in",
-      imgUrl: "/covidrescue.png",
-    },
-    {
-      title: "Algochurn",
-      link: "https://algochurn.com",
-      imgUrl: "/algochurn.png",
-    },
-    {
-      title: "Playground",
-      link: "https://play.placeholdertech.in",
-      imgUrl: "/playground.png",
-    },
-    {
-      title: "Feedmeback",
-      link: "https://feedmeback.vercel.app",
-      imgUrl: "/feedmeback.png",
-    },
-    {
-      title: "Devmedium",
-      link: "https://devmedium.vercel.app",
-      imgUrl: "/devmedium.png",
-    },
-  ],
-  about: {
-    title:
-      "I'm a software developer that loves building products and web applications that impact millions of lives",
-    description: [
-      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
-      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
-      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
-    ],
-    currentProject: "Tailwind Master Kit",
-    currentProjectUrl: "https://tailwindmasterkit.com",
-  },
-  experience: [
-    {
-      title: "Software Developer",
-      company: "mroads",
-      year: "2021",
-      companyLink: "https://mroads.com",
-      desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
-    },
-    {
-      title: "Founder and Developer",
-      company: "TailwindMasterKit",
-      year: "2021",
-      companyLink: "https://tailwindmasterkit.com",
-      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
-    },
-    {
-      title: "Founder and Developer",
-      company: "Algochurn",
-      year: "2021",
-      companyLink: "https://algochurn.com",
-      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
-    },
-    {
-      title: "Internship",
-      company: "Intel Corporation",
-      year: "2019",
-      companyLink: "https://intel.com",
-      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
-    },
-    {
-      title: "Graduation",
-      company: "UPES, Dehradun.",
-      year: "2019",
-      companyLink: "https://upes.co.in",
-      desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
-    },
-    {
-      title: "High School",
-      company: "Delhi Public School, Jaipur.",
-      year: "2015",
-      companyLink: "https://upes.co.in",
-      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
-    },
-  ],
-  resumeUrl:
-    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
-  socialLinks: {
-    instagram: "https://instagram.com/maninthere",
-    twitter: "https://twitter.com/mannupaaji",
-    linkedin: "https://linkedin.com/in/manuarora28",
-    github: "https://github.com/manuarora700",
-    facebook: "https://facebook.com/",
-  },
+	githubUsername: "Galaxeaaa",
+	name: "Cheng Wang",
+	designation: "Rendering Engine Developer",
+	avatarUrl: "/avatar.png",
+	email: "galaxea1111@gmail.com",
+	phone: "+86 13337730488",
+	address: "Hangzhou, Zhejiang, China.",
+	projects: [
+		{
+			title: "Real-time Subsurface Scattering",
+			link: "https://github.com/Galaxeaaa/burley-ss",
+			imgUrl: "/tmk.jpg",
+		},
+		// {
+		//   title: "Tailwind Master Kit",
+		//   link: "https://tailwindmasterkit.com",
+		//   imgUrl: "/tmk.jpg",
+		// },
+		// {
+		//   title: "PlaceholderTech",
+		//   link: "https://placeholdertech.in",
+		//   imgUrl: "/placeholdertech.png",
+		// },
+		// {
+		//   title: "Portfolio",
+		//   link: "https://manuarora.in",
+		//   imgUrl: "/portfolio.png",
+		// },
+		// {
+		//   title: "CovidRescue",
+		//   link: "https://covidrescue.co.in",
+		//   imgUrl: "/covidrescue.png",
+		// },
+		// {
+		//   title: "Algochurn",
+		//   link: "https://algochurn.com",
+		//   imgUrl: "/algochurn.png",
+		// },
+		// {
+		//   title: "Playground",
+		//   link: "https://play.placeholdertech.in",
+		//   imgUrl: "/playground.png",
+		// },
+		// {
+		//   title: "Feedmeback",
+		//   link: "https://feedmeback.vercel.app",
+		//   imgUrl: "/feedmeback.png",
+		// },
+		// {
+		//   title: "Devmedium",
+		//   link: "https://devmedium.vercel.app",
+		//   imgUrl: "/devmedium.png",
+		// },
+	],
+	about: {
+		title:
+			"I'm a senior student at Zhejiang University. I am a rendering engine developer intern that loves displaying a realistic world in the virtual space",
+		description: [
+			'Blablabla',
+			'Blablabla',
+			'Blablabla',
+		],
+		currentProject: "Real-time Subsurface Scattering",
+		currentProjectUrl: "https://github.com/Galaxeaaa/burley-ss",
+	},
+	experience: [
+		{
+			title: "Rendering Engine Developer Intern",
+			company: "RaysEngine",
+			year: "2021-2022",
+			companyLink: "https://mroads.com",
+			desc: "Contributed to subsurface scattering rendering in our engine.",
+		},
+		{
+			title: "Undergraduation",
+			company: "Zhejiang University",
+			year: "2018-2022",
+			companyLink: "https://www.zju.edu.cn",
+			desc: "Major in Computer Science and Technology with a GPA of 3.9.",
+		},
+		{
+			title: "High School",
+			company: "Yangzi High School Affiliated to Nanjing Normal University",
+			year: "2015-2018",
+			companyLink: "http://www.njyzzx.com/",
+			desc: "Normal student.",
+		},
+	],
+	resumeUrl:
+		"https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+	socialLinks: {
+		// instagram: "https://instagram.com/maninthere",
+		// twitter: "https://twitter.com/mannupaaji",
+		// linkedin: "https://linkedin.com/in/manuarora28",
+		github: "https://github.com/manuarora700",
+		// facebook: "https://facebook.com/",
+	},
 };
 
 export default userData;
