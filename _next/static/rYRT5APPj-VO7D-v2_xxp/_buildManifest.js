775ff861b678262de9203b1391ad337ea3915adf
Galaxeaaa <Galaxeaaa@users.noreply.github.com>
2022-10-12 19:38:11 +0000

    deploy: d04a086d8f400c55db207d1cd907caf43678a3c2

--- _next/static/rYRT5APPj-VO7D-v2_xxp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/814-2166eed6b9dc0c22.js","static/chunks/pages/index-1d9c190bbef3c6a7.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
