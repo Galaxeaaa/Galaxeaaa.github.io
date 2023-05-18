6feebc83bea4a4d24502eaf9f96332746f4bebe8
Cheng Wang <galaxea1111@gmail.com>
2023-05-17 23:58:13 -0700

    Update scroll using react-scroll.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Link from "next/link";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 import { useTheme } from "next-themes";
 import { useRouter } from "next/router";
 
@@ -18,26 +18,24 @@ export default function Navbar() {
 		<div className="bg-white/90 backdrop-blur border-b w-full transition-colors duration-500">
 			<div className="py-4 px-10 max-w-6xl mx-auto">
 				<div className="flex items-center text-lg font-normal space-x-2">
-					<Link href="#">
-						<a className="text-xl font-bold text-indigo-500 underline decoration-4 decoration-emerald-300 mr-4">
-							Cheng Wang
-						</a>
-					</Link>
-					<Link href="#about">
-						<a className="text-gray-700 hover:text-indigo-700 hover:bg-indigo-500/20 transition-all ease-in-out px-4 py-2 rounded-lg" >
+					<h onClick={() => scroll.scrollToTop({ duration: 1 })} className="cursor-pointer text-xl font-bold text-indigo-500 underline decoration-4 decoration-emerald-300 mr-4">
+						Cheng Wang
+					</h>
+					<ScrollLink to="about" spy={true} offset={-50}>
+						<h className="cursor-pointer text-gray-700 hover:text-indigo-700 hover:bg-indigo-500/20 transition-all ease-in-out px-4 py-2 rounded-lg" >
 							About
-						</a>
-					</Link>
-					<Link href="#projects">
-						<a className="text-gray-700 hover:text-indigo-700 hover:bg-indigo-500/20 transition-all ease-in-out px-4 py-2 rounded-lg" >
+						</h>
+					</ScrollLink>
+					<ScrollLink to="projects" spy={true} offset={-50}>
+						<h className="cursor-pointer text-gray-700 hover:text-indigo-700 hover:bg-indigo-500/20 transition-all ease-in-out px-4 py-2 rounded-lg" >
 							Projects
-						</a>
-					</Link>
-					<Link href="#experience">
-						<a className="text-gray-700 hover:text-indigo-700 hover:bg-indigo-500/20 transition-all ease-in-out px-4 py-2 rounded-lg" >
+						</h>
+					</ScrollLink>
+					<ScrollLink to="experience" spy={true} offset={-50}>
+						<h className="cursor-pointer text-gray-700 hover:text-indigo-700 hover:bg-indigo-500/20 transition-all ease-in-out px-4 py-2 rounded-lg" >
 							Experience
-						</a>
-					</Link>
+						</h>
+					</ScrollLink>
 				</div>
 				{/* <button
 					aria-label="Toggle Dark Mode"
