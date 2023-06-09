255ccdebaa3e77ba5f368d5cec91c3649aa6e992
Galaxeaaa <Galaxeaaa@users.noreply.github.com>
2023-06-09 02:43:26 +0000

    deploy: c1b7cbfa3ea99c86d362de08c0a2431327fbe005

---
+++ _next/static/mvnW6Haj1X3EwAVLFBmdF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-bbc953c444eab476.js","static/chunks/pages/index-12ea2dbab4640020.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
