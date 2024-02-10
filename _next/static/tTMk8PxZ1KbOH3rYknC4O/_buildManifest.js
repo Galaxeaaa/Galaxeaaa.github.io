b345245725b45c20f8ff3eff3b1042e249c1f99e
Galaxeaaa <Galaxeaaa@users.noreply.github.com>
2024-02-10 20:04:13 +0000

    deploy: 8bfa0754011125135eef7e50374ccc55c95e6368

--- _next/static/tTMk8PxZ1KbOH3rYknC4O/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-bbc953c444eab476.js","static/chunks/pages/index-32c06b701ef368b6.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
